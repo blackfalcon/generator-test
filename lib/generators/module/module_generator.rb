e1f4254cc22c1c070c22dcfc777b49468a6e0f9a
Dale Sande <dale.sande@gmail.com>
2013-02-14 08:27:06 -0800

    added example directory template

--- lib/generators/module/module_generator.rb
+++ lib/generators/module/module_generator.rb
@@ -1,7 +1,7 @@
 class ModuleGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_module
-    empty_directory 'app/views/shared/modules/foo/'
-    copy_file "module.sass", "app/assets/stylesheets/modules/foo/#{file_name}/_#{file_name}.sass"
+    directory "example", "app/views/shared/modules/#{file_name}"
+    copy_file "module.sass", "app/assets/stylesheets/modules/category/#{file_name}/_#{file_name}.sass"
   end
 end
