440a71e7149d00d5508c85cb38d6a0ad20e62f84
Dale Sande <dale.sande@gmail.com>
2013-02-14 07:25:38 -0800

    added new generator and template

--- lib/generators/module/module_generator.rb
+++ lib/generators/module/module_generator.rb
@@ -1,6 +1,7 @@
 class ModuleGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
-  def copy_module_file
-    copy_file "module.sass", "app/assets/stylesheets/#{file_name}.sass"
+  def create_module
+    empty_directory 'app/views/shared/modules/foo/'
+    copy_file "module.sass", "app/assets/stylesheets/modules/foo/#{file_name}/_#{file_name}.sass"
   end
 end
