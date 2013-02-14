cab0204a43d16a38a25e90356fb8748c9c2ce7bb
Dale Sande <dale.sande@gmail.com>
2013-02-14 08:30:18 -0800

    wip

--- lib/generators/module/module_generator.rb
+++ lib/generators/module/module_generator.rb
@@ -1,7 +1,7 @@
 class ModuleGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_module
-    directory "example", "app/views/shared/modules/#{file_name}"
+    directory "module_example_dir", "app/views/shared/modules/foo/#{file_name}"
     copy_file "module.sass", "app/assets/stylesheets/modules/category/#{file_name}/_#{file_name}.sass"
   end
 end
