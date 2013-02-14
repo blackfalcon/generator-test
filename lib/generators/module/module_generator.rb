9bf7ea696fa4c8155d17b4d53a368350f5d12eda
Dale Sande <dale.sande@gmail.com>
2013-02-14 10:41:26 -0800

    modules sass file

--- lib/generators/module/module_generator.rb
+++ lib/generators/module/module_generator.rb
@@ -1,9 +1,10 @@
+# This is looking pretty good, but the generator only takes one argument
+# `rails g module bar`  will generate the necessary module files
+# Currently hardcoded is the category directory of `foo`, need to make this an argument
+# As a user I want enter at the CLI `rails g module foo bar`
+
 class ModuleGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
-  # def create_haml_file
-  #   directory "module_example_dir", "app/views/shared/modules/foo/#{file_name}"
-  #   copy_file "module.html.haml", "app/views/shared/modules/foo/#{file_name}.html.haml"
-  # end
   def create_haml_module
     directory "example_haml_dir", "app/views/shared/modules/foo/"
     copy_file "module.html.haml", "app/views/shared/modules/foo/_#{file_name}.html.haml"
@@ -11,7 +12,11 @@ class ModuleGenerator < Rails::Generators::NamedBase
   def create_sass_module
     directory "example_sass_dir", "app/assets/stylesheets/modules/foo/#{file_name}"
     copy_file "module.sass", "app/assets/stylesheets/modules/foo/#{file_name}/_#{file_name}.sass"
-    #copy_file "_modules.sass", "app/assets/stylesheets/_modules.sass"
+
+    # Need this to happen, but if the file is already there it creates a conflict
+    # Need to either bypass the overwrite or silent the conflict alert
+    #create_file "app/assets/stylesheets/_modules.sass"
+
     append_to_file "app/assets/stylesheets/_modules.sass", "@import 'modules/foo/#{file_name}/#{file_name}'\n"
   end
 end
