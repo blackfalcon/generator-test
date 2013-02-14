387e2e36459583877799f40f96aeff5c9c822e1f
Dale Sande <dale.sande@gmail.com>
2013-02-14 09:02:46 -0800

    wip

--- lib/generators/module/module_generator.rb
+++ lib/generators/module/module_generator.rb
@@ -4,8 +4,12 @@ class ModuleGenerator < Rails::Generators::NamedBase
   #   directory "module_example_dir", "app/views/shared/modules/foo/#{file_name}"
   #   copy_file "module.html.haml", "app/views/shared/modules/foo/#{file_name}.html.haml"
   # end
-  def create_module
-    directory "module_example_dir", "app/assets/stylesheets/modules/foo/#{file_name}"
+  def create_haml_module
+    directory "example_haml_dir", "app/views/shared/modules/foo/"
+    copy_file "module.html.haml", "app/views/shared/modules/foo/_#{file_name}.sass"
+  end
+  def create_sass_module
+    directory "example_sass_dir", "app/assets/stylesheets/modules/foo/#{file_name}"
     copy_file "module.sass", "app/assets/stylesheets/modules/foo/#{file_name}/_#{file_name}.sass"
   end
 end
