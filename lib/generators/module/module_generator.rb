4166dee5813298d36c9a76c615602e39af4a0b88
Dale Sande <dale.sande@gmail.com>
2013-02-14 08:57:38 -0800

    more awesome to template dir

--- lib/generators/module/module_generator.rb
+++ lib/generators/module/module_generator.rb
@@ -1,7 +1,11 @@
 class ModuleGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+  # def create_haml_file
+  #   directory "module_example_dir", "app/views/shared/modules/foo/#{file_name}"
+  #   copy_file "module.html.haml", "app/views/shared/modules/foo/#{file_name}.html.haml"
+  # end
   def create_module
-    directory "module_example_dir", "app/views/shared/modules/foo/#{file_name}"
-    copy_file "module.sass", "app/assets/stylesheets/modules/category/#{file_name}/_#{file_name}.sass"
+    directory "module_example_dir", "app/assets/stylesheets/modules/foo/#{file_name}"
+    copy_file "module.sass", "app/assets/stylesheets/modules/foo/#{file_name}/_#{file_name}.sass"
   end
 end
